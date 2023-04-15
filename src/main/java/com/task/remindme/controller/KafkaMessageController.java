e78b441413bc7ad0a86ef65237229cf16376648f
Balaji <balajinandane3@gmail.com>
2023-04-15 10:57:30 +0530

    chages for send reminder to kafka

---
+++ src/main/java/com/task/remindme/controller/KafkaMessageController.java
@@ -0,0 +1,28 @@
+package com.task.remindme.controller;
+
+
+import com.task.remindme.entity.Reminder;
+import com.task.remindme.kafka.Producer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@Controller
+public class KafkaMessageController {
+
+    private final Producer producer;
+
+    public KafkaMessageController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping("/publish")
+    public void messageToTopic(@RequestBody Reminder reminder){
+        System.out.println("Kafka Message:"+reminder);
+
+    }
+
+
+}
