ef8c9606bbccadbe9896ba88a65ed5cb9a26ffca
Balaji <balajinandane3@gmail.com>
2023-05-09 16:32:43 +0530

    Changes for exception handler

--- src/main/java/com/task/remindme/controller/ReminderController.java
+++ src/main/java/com/task/remindme/controller/ReminderController.java
@@ -2,12 +2,18 @@ package com.task.remindme.controller;
 
 
 import com.task.remindme.entity.Reminder;
+import com.task.remindme.exception.ReminderExceptionController;
+import com.task.remindme.exception.ReminderNotFoundException;
 import com.task.remindme.service.ReminderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/reminders")
@@ -24,8 +30,12 @@ public class ReminderController {
 
     }
     @GetMapping("/{id}")
-    public Reminder findDepartmentById(@PathVariable("id") Long reminderId) throws Exception {
-        return reminderService.findByReminderId(reminderId);
+    public  ResponseEntity<Object> findDepartmentById(@PathVariable("id") Long reminderId) throws Exception {
+        Reminder reminder=reminderService.findByReminderId(reminderId);
+        if( reminder==null){
+            throw new ReminderNotFoundException("Reminder Id "+reminderId+" Is not present");
+        }
+        return ResponseEntity.ok(reminder);
     }
     @GetMapping("/")
     public List<Reminder> findAllDepartment(){
